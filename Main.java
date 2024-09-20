83c39e5204b04bb45cd96a8a003ab687d1df5a32
MalikMuhammadTayyabHassan <sp24-bse-057@cuilahore.edu.pk>
2024-09-20 15:47:29 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,84 @@
+class Artist {
+    private String name;
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void displayInformation() {
+        System.out.println("Artist Name: " + name);
+    }
+}
+
+class Artwork {
+    private String title;
+    private int year;
+    private Artist artist;
+
+    public Artwork(String title, int year, Artist artist) {
+        this.title = title;
+        this.year = year;
+        this.artist = artist;
+    }
+
+    public Artwork(String title, int year) {
+        this.title = title;
+        this.year = year;
+        this.artist = new Artist("Unknown Artist");
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void displayInformation() {
+        System.out.println("Artwork Title: " + title);
+        System.out.println("Year: " + year);
+        artist.displayInformation();
+    }
+
+    public Artwork shallowCopy() {
+        return new Artwork(this.title, this.year, this.artist);
+    }
+
+    public Artwork deepCopy() {
+        return new Artwork(this.title, this.year, new Artist(this.artist.getName()));
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Artist artist1 = new Artist("Leonardo da Vinci");
+
+        Artwork artwork1 = new Artwork("Mona Lisa", 1503, artist1);
+        Artwork artwork2 = new Artwork("The Last Supper", 1498);
+
+        System.out.println("Original Artworks:");
+        artwork1.displayInformation();
+        artwork2.displayInformation();
+
+        Artwork shallowCopy = artwork1.shallowCopy();
+        Artwork deepCopy = artwork1.deepCopy();
+
+        artist1 = new Artist("Vincent van Gogh");
+
+        System.out.println("\nAfter Modifying Artist Name:");
+        artwork1.displayInformation();
+        System.out.println("\nShallow Copy:");
+        shallowCopy.displayInformation();
+        System.out.println("\nDeep Copy:");
+        deepCopy.displayInformation();
+    }
+}
